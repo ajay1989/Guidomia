1c2b05453c7d14d93321d5325a674e4fbd03a928
Ajay Vyas <ajay.vays@octalsoftware.net>
2021-10-30 15:20:40 +0530

    Inital Project setup

---
+++ Guidomia/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia
+//
+//  Created by Ajay Vyas on 30/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Guidomia/Utility/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  Guidomia
+//
+//  Created by Ajay Vyas on 30/10/21.
+//
+
+import Foundation
